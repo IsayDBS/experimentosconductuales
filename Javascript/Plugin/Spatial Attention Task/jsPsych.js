24e990a771d486067707d237b81e18136d7c9d2d
Isay Balderas <isaydamar@ciencias.unam.mx>
2023-08-16 00:16:39 -0600

    Final

--- Javascript/Plugin/Spatial Attention Task/jsPsych.js
+++ Javascript/Plugin/Spatial Attention Task/jsPsych.js
@@ -1,3 +1,8 @@
+/*
+*La tarea de atención espacial, se presenta una flecha apuntando a una dirección (arriba, abajo, izquierda, derecha)
+*, despues de este, aparecerá una imagen, en cualquiera de estas direcciones
+*al azar, el objetivo del participante es apretar la dirección en la que apareció la imagen 
+*/
 var jsPsych = initJsPsych({
     on_finish: function(){
         jsPsych.data.get().localSave('csv','informacion.csv');
